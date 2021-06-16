6fc5a4bcd8b8408e87e0051ee1e30d6a032633f1
Sirapavee <sirapavee@gmail.com>
2021-06-16 19:17:06 +0700

    initial

---
+++ components/navbar.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/NavigationBar.module.scss'
+
+export default function NavigationBar() {
+    return (
+        <section className={styles.container}>
+            
+            <p>The New York Times</p>
+        </section>
+    )
+}
